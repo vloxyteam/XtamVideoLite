3123e0dbcdcc792274fa669803b342640bf2b708
Xlerion <45642537+xlerionultimate@users.noreply.github.com>
2020-10-26 13:18:57 -0500

    Update buscarregistrarcamara2.py

--- buscarregistrarcamara2.py
+++ buscarregistrarcamara2.py
@@ -7,17 +7,53 @@ import glob
 import listarcam2 as lst2
 import sys, codecs
 import locale
-import revisartamano2 as rt2
+import revisartamano2 as tam
+import daystart as ds
+import timestart as tis
+import duracion as dur
+import datefinish as df
+import timefinish as tf
+import datetimestart as dts
 
 print (sys.getdefaultencoding())
 
-ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#1 nombre del archivo - filename
 filename = lst2.latest_file2
+
+#2 obtener el tamaño del archivo - size
+tam = tam.os.path.getsize(filename) 
+#print('El tamaño es de: ', tam, 'bytes')
+
+#3 comienzo del tiempo - datestart
+ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+df = df.get_duration(filename)
+
+#4 comienzo de tiempo - timestart
+dts = dts.x
+
+#5 Tiempo de inicio del video
+tiv = tis
+
+#6 listados de los archivos ts
 dn = lst2.latest_file2
 
-# get the size of file
-tam = os.path.getsize(filename) 
-#print('El tamaño es de: ', tam, 'bytes')
+#7 Dia Final
+
+
+#8 duracion del archivo
+dt = dur.get_duration(filename)
+
+#7 tiempo final del archivo
+tf = tf.x
+
+#8 Dia de modificación
+di = os.stat('/').st_mtime
+
+#9 Tiempo de modificacion
+hi = os.stat('/').st_ctime
+
+
 
 
 conn = mariadb.connect(
@@ -31,19 +67,17 @@ cur = conn.cursor()
 
 def result():
     resultado = [y for x in os.walk("") for y in glob(os.path.join (x[0], '*.ts'))]
- #   print ("Resultado : %s" % resultado)
+#   print ("Resultado : %s" % resultado)
 
 nom = dir
 
 try:
-    cur.execute("INSERT INTO recordings (id, filename, type, size, datestart, timestart, datefinish, timefinish, idCamara, datetimestart, datetimefinish) VALUES ( NULL, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?)", (filename, '.ts', tam, ts, ts, ts, ts, 2, ts, ts))
+    cur.execute("INSERT INTO recordings (id, filename, type, size, datestart, timestart, datefinish, timefinish, idCamara, datetimestart, datetimefinish) VALUES ( NULL, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?)", (filename, '.ts', tam, ts, ts, dts, df, 2, ts, tf))
 
 except mariadb.Error as e:
     print(f"Error: {e}")
   
 # Finalizar 
 conn.commit()
-print (f"Último Insertado desde camara2: {cur.lastrowid}")
+print (f"Último Insertado desde camara 2: {cur.lastrowid}")
 conn.close()
-
-
