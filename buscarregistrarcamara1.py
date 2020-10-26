903f9d6c1bb79e9209fc5cba1c4a9187f66760ba
Xlerion <45642537+xlerionultimate@users.noreply.github.com>
2020-10-26 11:57:34 -0500

    Update buscarregistrarcamara1.py
    Actualización - 1

--- buscarregistrarcamara1.py
+++ buscarregistrarcamara1.py
@@ -7,17 +7,52 @@ import glob
 import listarcam1 as lst1
 import sys, codecs
 import locale
-import revisartamano1 as rt1
+import revisartamano1 as tam
+import daystart as ds
+import timestart as ts
+import duracion as dur
+import datefinish as df
+import timefinish as tf
+import datetimestart as dts
 
 print (sys.getdefaultencoding())
 
-ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#1 nombre del archivo - filename
 filename = lst1.latest_file
+
+#2 obtener el tamaño del archivo - size
+tam = tam.os.path.getsize(filename) 
+#print('El tamaño es de: ', tam, 'bytes')
+
+#3 comienzo del tiempo - datestart
+ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+#4 comienzo de tiempo - timestart
+dts = dts.x
+
+#5 Tiempo de inicio del video
+tiv = ts
+
+#6 listados de los archivos ts
 dn = lst1.latest_file
 
-# get the size of file
-tam = rt1.os.path.getsize(filename) 
-#print('El tamaño es de: ', tam, 'bytes')
+#7 Dia Final
+
+
+#8 duracion del archivo
+dt = dur.get_duration(filename)
+
+#7 tiempo final del archivo
+tf = tf.get_duration(filename)
+
+#8 Dia de modificación
+di = os.stat('/').st_mtime
+
+#9 Tiempo de modificacion
+hi = os.stat('/').st_ctime
+
+
 
 
 conn = mariadb.connect(
@@ -36,7 +71,7 @@ def result():
 nom = dir
 
 try:
-    cur.execute("INSERT INTO recordings (id, filename, type, size, datestart, timestart, datefinish, timefinish, idCamara, datetimestart, datetimefinish) VALUES ( NULL, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?)", (filename, '.ts', tam, ts, ts, ts, ts, 1, ts, ts))
+    cur.execute("INSERT INTO recordings (id, filename, type, size, datestart, timestart, datefinish, timefinish, idCamara, datetimestart, datetimefinish) VALUES ( NULL, ? , ?, ?, ?, ?, ?, ?, ?, ?, ?)", (filename, '.ts', tam, ts, ts, ts, tf, 1, dts, ts))
 
 except mariadb.Error as e:
     print(f"Error: {e}")
@@ -45,5 +80,3 @@ except mariadb.Error as e:
 conn.commit()
 print (f"Último Insertado desde camara 1: {cur.lastrowid}")
 conn.close()
-
-
